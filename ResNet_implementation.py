a86aa152772ccda0bbdd87038bef5ee3d3885bf0
Callum <callum.leung@gmail.com>
2019-07-31 13:26:32 +0100

    removed check for id

--- ResNet_implementation.py
+++ ResNet_implementation.py
@@ -68,12 +68,11 @@ def load_images(images_csv_path, images_path):
     #images = [tf.read_file(images_path/file) for file in all_images_id_extension]
     images = []
     for file in images_to_load:
-        if file != 'id':
-            print(images_to_load.head())
-            path = '{}/{}'.format(images_path, file)
-            print(path)
-            images.append(image.load_img(path))
-        
+        print(images_to_load.head())
+        path = '{}/{}'.format(images_path, file)
+        print(path)
+        images.append(image.load_img(path))
+
     return pd.DataFrame(images, columns=['images'])
 
 
